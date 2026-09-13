751cf8157f4d65bb952e7470a3fef20d618f8e96
Armaan Bains <110433296+armaanbains1@users.noreply.github.com>
2026-09-12 20:30:44 -0400

    Refactor argmax module for improved functionality

--- vFiles/argmax.sv
+++ vFiles/argmax.sv
@@ -1,25 +1,24 @@
 module argmax(
-input CLOCK_50, 
-input signed [27:0] layer2 [9:0], 
-input trigger, 
-output reg [3:0] class_out
+	input CLOCK_50, 
+	input signed [26:0] layer2 [9:0], 
+	input trigger, 
+	output reg [3:0] class_out
 );
-	
 	reg [3:0] maxNum;
-	reg signed [27:0] maxVal;
-	int i;
+	reg signed [26:0] maxVal;
+	integer i;
+
 	always @(posedge CLOCK_50) begin
 		if (trigger) begin
-				maxNum = 4'd0;
-				maxVal = layer2[0];
-				for (i = 0; i <= 9; i = i + 1) begin
-					if (layer2[i] > maxVal) begin
-						maxNum = i;
-						maxVal = layer2[i];
-					end
+			maxNum = 4'd0;
+			maxVal = layer2[0];
+			for (i = 1; i < 10; i = i + 1) begin
+				if (layer2[i] > maxVal) begin
+					maxNum = i[3:0];
+					maxVal = layer2[i];
 				end
-				class_out <= maxNum;
+			end
+			class_out <= maxNum;
 		end
 	end
-	
 endmodule
